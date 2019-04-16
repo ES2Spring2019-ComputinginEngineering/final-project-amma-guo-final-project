96d6c80af9d37f57df2c133f0d41a3f8fef13097
Amma Agyei <agyeiamma@gmail.com>
2019-04-16 16:01:39 -0400

    Update base.py

--- base.py
+++ base.py
@@ -6,8 +6,43 @@ Created on Tue Apr 16 15:46:21 2019
 """
 
 import numpy as np
-
+import csv
 
 def readdatafile():
+    csv_file = open('Parkinsonsdiseasedataonlyz.csv')
+    total_row = sum(1 for row in csv_file)
+    csv_file.seek(0)
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    #Data Parsing and creating variables
+    height  = np.zeros((total_row - 1,))                                 #empty array of zeros
+    age  = np.zeros((total_row - 1,))
+    weight = np.zeros((total_row - 1,))
+    group = np.zeros((total_row-1,))
+    index = 0
+
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are '+ str(row))
+        elif line_count > 0:
+            height[index] = (float(row[7]))                   #normalizing of data 
+            age[index] = (float(row[6]))
+            weight[index] = (float(row[8]))
+            index += 1
+        line_count += 1
     
-def pot
+     #Trimming
+  #  blood_glucose = blood_glucose[:index]                                      #trimming data since last element in the arrays is 0
+   # hemoglobin = hemoglobin[:index]
+    #classification = classification[:index]
+    
+    print('Processed ' +str(line_count)+' lines.')
+    print(str(index)+' lines meet conditions.')
+    csv_file.close()     
+
+    return height, weight, age
+
+height,weight,age = readdatafile()
+
+#def pot
